f75ce09f3e905d7927c26589f2c1c26da77b7e97
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-01-21 20:59:30 -0500

    feat (hyprland): set color vars from nix-colors

--- home-manager/common/gui/hypr/hypr-settings/theme.nix
+++ home-manager/common/gui/hypr/hypr-settings/theme.nix
@@ -2,11 +2,22 @@
 
 {
   wayland.windowManager.hyprland.settings = {
+    "$color-gray0"  = "0xff${config.colorScheme.colors.base05}";
+    "$color-gray1"  = "0xff${config.colorScheme.colors.base03}";
+    "$color-gray2"  = "0xff${config.colorScheme.colors.base07}";
+    "$color-red"    = "0xff${config.colorScheme.colors.base08}";
+    "$color-yellow" = "0xff${config.colorScheme.colors.base09}";
+    "$color-tan"    = "0xff${config.colorScheme.colors.base0A}";
+    "$color-dteal"  = "0xff${config.colorScheme.colors.base0B}";
+    "$color-lteal"  = "0xff${config.colorScheme.colors.base0C}";
+    "$color-purple" = "0xff${config.colorScheme.colors.base0D}";
 
     general = {
       "gaps_in" = "7";
       "gaps_out" = "8";
       "border_size" = "2";
+      "col.active_border" = "$color-yellow";
+      "col.inactive_border" = "$color-gray2";
     };
 
     decoration = {
