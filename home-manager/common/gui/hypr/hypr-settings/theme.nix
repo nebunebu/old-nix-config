5a12c949650adc8dc8d8ff642a74df9e6290d564
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-01-23 09:28:36 -0500

    feat(hyprland): add border decorations for float and full

--- home-manager/common/gui/hypr/hypr-settings/theme.nix
+++ home-manager/common/gui/hypr/hypr-settings/theme.nix
@@ -1,24 +1,26 @@
 { config, ...}:
 
 {
-  # NOTE: cleaner to use `opt = with config.colorScheme.colors; {...};`
-  wayland.windowManager.hyprland.settings = {
-    "$color-gray0"  = "0xff${config.colorScheme.colors.base05}";
-    "$color-gray1"  = "0xff${config.colorScheme.colors.base03}";
-    "$color-gray2"  = "0xff${config.colorScheme.colors.base07}";
-    "$color-red"    = "0xff${config.colorScheme.colors.base08}";
-    "$color-yellow" = "0xff${config.colorScheme.colors.base09}";
-    "$color-tan"    = "0xff${config.colorScheme.colors.base0A}";
-    "$color-dteal"  = "0xff${config.colorScheme.colors.base0B}";
-    "$color-lteal"  = "0xff${config.colorScheme.colors.base0C}";
-    "$color-purple" = "0xff${config.colorScheme.colors.base0D}";
+  wayland.windowManager.hyprland.settings 
+  = with config.colorScheme.colors; {
+    "$color-gray0"  = "0xff${base05}";
+    "$color-gray1"  = "0xff${base03}";
+    "$color-gray2"  = "0xff${base07}";
+    "$color-red"    = "0xff${base08}";
+    "$color-yellow" = "0xff${base09}";
+    "$color-tan"    = "0xff${base0A}";
+    "$color-dteal"  = "0xff${base0B}";
+    "$color-lteal"  = "0xff${base0C}";
+    "$color-purple" = "0xff${base0D}";
 
     general = {
       "gaps_in" = "7";
       "gaps_out" = "8";
       "border_size" = "2";
+      # "col.active_border" = "$color-yellow $color-dteal 90deg";
       "col.active_border" = "$color-yellow";
       "col.inactive_border" = "$color-gray2";
+      "bezier" = "linear, 0.0, 0.0, 1.0, 1.0";
     };
 
     decoration = {
@@ -29,6 +31,24 @@
 
     animations = {
       "enabled" = "yes";
+      # "bezier" = [
+      #   "myBezier, 0.05, 0.9, 0.1, 1.05"
+      # ];
+      # "animation" = [
+      #   # "windows, 1, 7, myBezier"
+      #   "borderangle, 1, 70, linear, loop"
+      #   # "windowsOut, 1, 7, default, popin 80%"
+      #   # "border, 1, 10, default"
+      #   # "borderangle, 1, 8, default"
+      #   # "fade, 1, 7, default"
+      #   # "workspaces, 1, 6, default"
+      # ];
     };
+
+    windowrulev2 = [
+      "bordercolor $color-purple, fullscreen:1"
+      "bordercolor $color-lteal, floating:1" 
+      "bordercolor $color-red, pinned:1" 
+    ];
   };
 }
