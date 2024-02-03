5b3c56f1f9d8ad2df61ca2659f4cacd9d83b253f
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-02-03 04:09:53 -0500

    clean up zsh

--- home-manager/common/non-gui/zsh/default.nix
+++ home-manager/common/non-gui/zsh/default.nix
@@ -21,9 +21,9 @@
           # fi
         '';
 
-      # localVariables = {
-      #   CLIPBOARD = "wlp";
-      # };
+      localVariables = {
+        CLIPBOARD = "wlp";
+      };
       initExtra =
         builtins.readFile ./zsh-fzf-kill.zsh
         # + builtins.readFile ./zsh-clipboard.zsh
@@ -106,25 +106,6 @@
             sha256 = "sha256-xbchXJTFWeABTwq6h4KWLh+EvydDrDzcY9AQVK65RS8=";
           };
         }
-        # {
-        #   name = "zsh-system-clipboard";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "kutsan";
-        #     repo = "zsh-system-clipboard";
-        #     rev = "v0.8.0";
-        #     sha256 = "sha256-VWTEJGudlQlNwLOUfpo0fvh0MyA2DqV+aieNPx/WzSI=";
-        #   };
-        # }
-        # {
-        #   name = "zsh-nix-shell";
-        #   file = "nix-shell.plugin.zsh";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "chisui";
-        #     repo = "zsh-nix-shell";
-        #     rev = "v0.7.0";
-        #     sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
-        #   };
-        # }
       ];
     };
   };
