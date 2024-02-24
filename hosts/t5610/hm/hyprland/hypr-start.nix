4d92e6b2a5f3f95ca61552a256198dc68594c285
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-02-24 14:11:33 -0500

    add ags and swww to hyprland init script

--- hosts/t5610/hm/hyprland/hypr-start.nix
+++ hosts/t5610/hm/hyprland/hypr-start.nix
@@ -1,12 +1,14 @@
 { pkgs, config, ... }: {
   home.packages = [
+    pkgs.swww
     (pkgs.writeShellScriptBin "hypr-start" ''
       # /nix/store/$(ls -la /nix/store | grep polkit-kde-agent | grep '^d' | awk '{print $9}')/libexec/polkit-kde-authentication-agent-1 &
-      ${config.home.homeDirectory}/.nix-profile/bin/pypr
-      ${pkgs.swww}/bin/swww init
       # ${pkgs.waybar}/bin/waybar &
-      ${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store
-      ${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store
+      # ${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store
+      ${config.home.homeDirectory}/.nix-profile/bin/pypr &
+      ${config.home.homeDirectory}/.nix-profile/bin/ags &
+      ${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store &
+      ${pkgs.swww}/bin/swww init
     '')
   ];
 }
