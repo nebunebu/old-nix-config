7d28358cd32c666d70512325e7bc0c13d08eb9a5
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-02-25 09:50:44 -0500

    add lf preview (has bugs)

--- home-manager/programs/lf/default.nix
+++ home-manager/programs/lf/default.nix
@@ -1,5 +1,31 @@
+{ pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    ctpv
+    file
+    poppler_utils
+    ffmpegthumbnailer
+    atool
+  ];
   programs.lf = {
     enable = true;
+    commands = {
+      # NOTE: cleaner not running correctly
+      clearpreview = "\$${pkgs.ctpv}/bin/ctpvclear >/dev/null 2>&1";
+    };
+    keybindings = {
+      c = "clearpreview";
+    };
+    previewer = {
+      source = "${pkgs.ctpv}/bin/ctpv";
+    };
+    settings = {
+      forcekitty = true;
+      shell = "${pkgs.bash}/bin/bash";
+      cleaner = "${pkgs.ctpv}/bin/ctpvclear";
+    };
+    extraConfig = ''
+      cmd on-quit %${pkgs.ctpv}/bin/ctpv -e $id
+    '';
   };
 }
