458dcfc7859ab8bb6d65ba473260031f821d50e7
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-02-26 05:28:04 -0500

    refactor firefox profiles

--- home-manager/programs/firefox/default.nix
+++ home-manager/programs/firefox/default.nix
@@ -1,68 +1,58 @@
 { inputs, ... }:
+let
+  ffao = [
+    inputs.firefox-addons.packages."x86_64-linux".ublock-origin
+    inputs.firefox-addons.packages."x86_64-linux".vimium
+  ];
+in
 {
   programs.firefox = {
     enable = true;
-    profiles.nebu = {
-      isDefault = true;
-      id = 0;
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        vimium
-        i-dont-care-about-cookies
-        reddit-enhancement-suite
-        old-reddit-redirect
-      ];
-      userChrome =
-        /*
-      css
-        */
-        ''
-          * {
-            font-family: "DroidSansM Nerd Font" !important;
-            font-family: "DroidSansM Nerd Font Mono" !important;
-          };
-        '';
-
-    };
-    profiles.noogle = {
-      id = 1;
-      settings = {
-        "browser.startup.homepage" = "https://noogle.dev";
+    profiles = {
+      nebu = {
+        isDefault = true;
+        id = 0;
+        extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+          i-dont-care-about-cookies
+          reddit-enhancement-suite
+          old-reddit-redirect
+        ] ++ ffao;
+        userChrome = /* css */
+          ''
+            * {
+              font-family: "DroidSansM Nerd Font" !important;
+              font-family: "DroidSansM Nerd Font Mono" !important;
+            };
+          '';
       };
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        vimium
-      ];
-    };
-    profiles.nixpkgs = {
-      id = 2;
-      settings = {
-        "browser.startup.homepage" = "https://search.nixos.org/packages";
+      noogle = {
+        id = 1;
+        settings = {
+          "browser.startup.homepage" = "https://noogle.dev";
+        };
+        extensions = ffao;
       };
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        vimium
-      ];
-    };
-    profiles.hm-options = {
-      id = 3;
-      settings = {
-        "browser.startup.homepage" = "https://mipmip.github.io/home-manager-option-search";
+      nixpkgs = {
+        id = 2;
+        settings = {
+          "browser.startup.homepage" = "https://search.nixos.org/packages";
+        };
+        extensions = ffao;
       };
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        vimium
-      ];
-    };
-    profiles.lichess = {
-      id = 4;
-      settings = {
-        "browser.startup.homepage" = "https://lichess.org/training";
+      hm-options = {
+        id = 3;
+        settings = {
+          "browser.startup.homepage" = "https://mipmip.github.io/home-manager-option-search";
+        };
+        extensions = ffao;
       };
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        vimium
-      ];
+      lichess = {
+        id = 4;
+        settings = {
+          "browser.startup.homepage" = "https://lichess.org/training";
+        };
+        extensions = ffao;
+      };
     };
   };
 }
