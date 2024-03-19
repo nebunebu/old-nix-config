0921065ced59b8e56be8b707638d1ae3fa4b7446
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-03-19 14:25:37 -0400

    remove hydra flake input

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    hydra.url = "github:nixos/hydra";
 
     disko = {
       url = "github:nix-community/disko";
