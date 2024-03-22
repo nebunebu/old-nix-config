656953beeb62bf870725d4bdca1a0b59ff762668
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-03-22 03:00:09 -0400

    refactor

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
     hardware.url = "github:nixos/nixos-hardware";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
     solaar.url = "github:Svenum/Solaar-Flake/latest";
     hydra.url = "github:nixos/hydra";
     disko = {
